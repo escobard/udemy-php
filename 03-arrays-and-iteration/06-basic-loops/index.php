e82fc10a292724d2c6ac0168072a7e936794129b
escobard <escobard90@gmail.com>
2025-06-11 10:05:24 -0600

    feat: basic loops

--- 03-arrays-and-iteration/06-basic-loops/index.php
+++ 03-arrays-and-iteration/06-basic-loops/index.php
@@ -1,3 +1,27 @@
+<?php
+
+// basic PHP for loop syntax
+for ($i = 0; $i < 10; $i++) {
+    echo $i . '<br />';
+};
+
+// basic PHP while loop syntax
+$i = 0;
+
+while ($i < 10) {
+    echo $i . '<br />';
+    $i++;
+};
+
+// basic do while loops - dont think I have used these before
+/// do while loops run the loop logic first and THEN check if the condition has been met - this is different than for or while loops, which check the condition prior to running the loop logic
+
+do {
+    echo $i;
+    $i++;
+} while ($i < 10)
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,6 +41,18 @@
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
+            <ul>
+                <!-- Another way of using a for loop in PHP -->
+                <?php for ($i = 0; $i <= 10; $i++): ?>
+                    <li>Number: <?= $i ?></li>
+                <?php endfor; ?>
+                <!-- Another way of using a while loop in PHP -->
+                <?php $i = 0;
+                while ($i < 10): ?>
+                    <li>Number: <?= $i ?></li>
+                <?php $i++;
+                endwhile; ?>
+            </ul>
         </div>
     </div>
 </body>
