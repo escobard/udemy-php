35ca05e3b9392eb5417f58debf2c0771a33e6158
escobard <escobard90@gmail.com>
2025-06-14 10:49:53 -0600

    feat: looping through multi dimensional arrays

--- section03-arrays-and-iteration/09-multi-dimensional-array-iteration/index.php
+++ section03-arrays-and-iteration/09-multi-dimensional-array-iteration/index.php
@@ -61,46 +61,27 @@ $listings = [
     </div>
   </header>
   <div class="container mx-auto p-4 mt-4">
-    <!-- Output -->
-    <div class="md my-4">
-      <div class="bg-white rounded-lg shadow-md">
-        <div class="p-4">
-          <h2 class="text-xl font-semibold">Software Engineer</h2>
-          <p class="text-gray-700 text-lg mt-2">We are seeking a skilled software engineer to develop high-quality software solutions.</p>
-          <ul class="mt-4">
-            <li class="mb-2">
-              <strong>Salary:</strong> $80,000
-            </li>
-            <li class="mb-2">
-              <strong>Location:</strong> San Francisco
-            </li>
-            <li class="mb-2">
-              <strong>Tags:</strong> Software Development, Java, Python
-            </li>
-          </ul>
+    <?php foreach ($listings as $listing) : ?>
+      <div class="md my-4">
+        <div class="bg-white rounded-lg shadow-md">
+          <div class="p-4">
+            <h2 class="text-xl font-semibold"><?= $listing['title'] ?></h2>
+            <p class="text-gray-700 text-lg mt-2"><?= $listing['description'] ?></p>
+            <ul class="mt-4">
+              <li class="mb-2">
+                <strong>Salary:</strong> <?= $listing['salary'] ?>
+              </li>
+              <li class="mb-2">
+                <strong>Location:</strong> <?= $listing['location'] ?>
+              </li>
+              <li class="mb-2">
+                <strong>Tags: </strong><?= implode(", ", $listing['tags']) ?>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div class="md my-4">
-      <div class="bg-white rounded-lg shadow-md">
-        <div class="p-4">
-          <h2 class="text-xl font-semibold">Marketing Specialist</h2>
-          <p class="text-gray-700 text-lg mt-2">We are looking for a marketing specialist to develop and implement effective marketing strategies.</p>
-          <ul class="mt-4">
-            <li class="mb-2">
-              <strong>Salary:</strong> $60,000
-            </li>
-            <li class="mb-2">
-              <strong>Location:</strong> New York
-            </li>
-            <li class="mb-2">
-              <strong>Tags:</strong> Digital Marketing, Social Media, SEO
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <?php endforeach ?>
   </div>
 </body>
 
