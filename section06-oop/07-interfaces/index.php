1d9a271873a6f47d34197dbdeb27520c19542d64
escobard <escobard90@gmail.com>
2025-06-20 16:57:59 -0600

    feat: interfaces

--- section06-oop/07-interfaces/index.php
+++ section06-oop/07-interfaces/index.php
@@ -1,3 +1,66 @@
+<?php
+
+// interface defines a shape a class must follow
+interface ContentInterface
+{
+  public function display();
+
+  public function edit();
+}
+
+class Article implements ContentInterface
+{
+  private $title;
+  private $content;
+
+  public function __construct($title, $content)
+  {
+    $this->title = $title;
+    $this->content = $content;
+  }
+
+  // implementing class must have parent interface methods
+  public function display()
+  {
+    echo "<h2>{$this->title}</h2>";
+    echo "<p>{$this->content}</p>";
+  }
+
+  public function edit()
+  {
+    echo "Editing the article '{$this->title}'";
+  }
+}
+
+class Video implements ContentInterface
+{
+  private $title;
+  private $url;
+
+  public function __construct($title, $url)
+  {
+    $this->title = $title;
+    $this->url = $url;
+  }
+
+  // implementing class must have parent interface methods
+  public function display()
+  {
+    echo "<h2>{$this->title}</h2>";
+    echo "<iframe src='{$this->url}'/>";
+  }
+
+  public function edit()
+  {
+    echo "Editing the video '{$this->title}'";
+  }
+}
+
+$article = new Article('Intro', 'Test content');
+$video = new Video('Intro', 'http://localhost:3000');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,6 +80,11 @@
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
       <!-- Output -->
+      <?= $article->display() ?>
+    </div>
+    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+      <!-- Output -->
+      <?= $video->display() ?>
     </div>
   </div>
 </body>
