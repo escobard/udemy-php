5deb87d2532de57bb85f0ef8cf97b29566d3f98b
escobard <escobard90@gmail.com>
2025-06-25 10:15:59 -0600

    feat: basic database connections with PDO

---
+++ section08-database/07-connect-pdo/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// https://www.php.net/manual/en/class.pdo.php
+// use PHP PDO class to connect your PHP application to a database server
+$host = 'localhost';
+$port = 3306;
+$dbName = 'blog';
+$username = 'phpapp';
+$password = 'phpapp';
+
+// string that with associated dta structure used to describe a connection to a data source
+/// https://en.wikipedia.org/wiki/Data_source_name 
+$dsn = "mysql:host={$host};port={$port};dbname={$dbName};charset=utf8";
+
+// basic try/catch syntax with PHP
+try {
+  // create PDO instance
+  $pdo = new PDO($dsn, $username, $password);
+
+  // set PDO to throw exceptions on error
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo 'Database connected...';
+} catch (PDOException $e) {
+  echo 'Connection Failed: ' . $e->getMessage();
+}
