5525884358c1c019e20c06e3e67e35e63e5776ea
escobard <escobard90@gmail.com>
2025-06-25 10:59:26 -0600

    feat: use url headers to fetch data from database, redirect based on data fetched from database

--- section08-database/09-select-single-record/database.php
+++ section08-database/09-select-single-record/database.php
@@ -1,22 +1,27 @@
 <?php
 
-// Database configuration
+// https://www.php.net/manual/en/class.pdo.php
+// use PHP PDO class to connect your PHP application to a database server
 $host = 'localhost';
 $port = 3306;
 $dbName = 'blog';
-$username = 'root';
-$password = '';
+$username = 'phpapp';
+$password = 'phpapp';
 
-// Connection string (DSN)
+// string that with associated dta structure used to describe a connection to a data source
+/// https://en.wikipedia.org/wiki/Data_source_name 
 $dsn = "mysql:host={$host};port={$port};dbname={$dbName};charset=utf8";
 
+// basic try/catch syntax with PHP
 try {
-  // Create a PDO instance
+  // create PDO instance
   $pdo = new PDO($dsn, $username, $password);
 
-  // Set PDO to throw exceptions on error
+  // set PDO to throw exceptions on error
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // set pdo attribute to only return associative arrays
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-  // If there is an error with the connection, catch it here
-  echo "Connection failed: " . $e->getMessage();
+  echo 'Connection Failed: ' . $e->getMessage();
 }
--- section08-database/09-select-single-record/post.php
+++ section08-database/09-select-single-record/post.php
@@ -1,3 +1,31 @@
+<?php
+
+require 'database.php';
+
+// the $_GET superglobal can be used to fetch query parameters from URL
+$id = $_GET['id'] ?? null;
+
+if (!$id) {
+
+  // redirects the user back to index.php
+  /// example of a basic redirect with PHP
+  header('Location: index.php');
+  exit;
+}
+
+// bad approach, prone to sql injections
+// $sql = 'SELECT * FROM posts WHERE id = ' . $id;
+
+// better approach - prevents straight injections by converting arguments to strings
+$sql = 'SELECT * FROM posts WHERE id = :id';
+$stmt = $pdo->prepare($sql);
+$params = ['id' => $id];
+$stmt->execute($params);
+
+// fetches a single record with pdo
+$post = $stmt->fetch();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +33,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
-  <title>Post One</title>
+  <title><?= $post['title'] ?> </title>
 </head>
 
 <body class="bg-gray-100">
@@ -18,8 +46,8 @@
     <div class="md my-4">
       <div class="rounded-lg shadow-md">
         <div class="p-4">
-          <h2 class="text-xl font-semibold">Post One</h2>
-          <p class="text-gray-700 text-lg mt-2 mb-5">This is post one</p>
+          <h2 class="text-xl font-semibold"><?= $post['title'] ?></h2>
+          <p class="text-gray-700 text-lg mt-2 mb-5"><?= $post['body'] ?></p>
           <a href="index.php">Go Back</a>
         </div>
       </div>
