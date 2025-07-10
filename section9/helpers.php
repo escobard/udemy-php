6ccdc56e868bfabd0a223790c96c879087437e24
escobard <escobard90@gmail.com>
2025-07-10 15:58:43 -0600

    feat: home page setup

---
+++ section9/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Get the root path for the project
+ * 
+ * @param string $path
+ * @return string 
+ */
+
+function basePath($path = '')
+{
+  return __DIR__ . '/' . $path;
+}
---
+++ section9/public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+// imports all the content from a specific file
+require '../helpers.php';
+
+// code from that file then becomes available for use - as shown with basePath function
+require basePath('views/home.view.php');
