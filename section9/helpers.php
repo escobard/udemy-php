e9bc3a7437cd5d31e8da624a90a67b86363197f8
escobard <escobard90@gmail.com>
2025-07-11 17:00:01 -0600

    feat: log helpers

--- section9/helpers.php
+++ section9/helpers.php
@@ -43,3 +43,31 @@ function loadPartial($name)
     echo "Partial {$partialPath} not found!'";
   }
 }
+
+/**
+ * Inspect value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '<pre>';
+}
+
+
+/**
+ * Inspect value(s) then die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '<pre>';
+  die();
+}
