4435ffe5d38d828b7f029a5c481889246034dacd
escobard <escobard90@gmail.com>
2025-07-11 17:14:28 -0600

    feat: basic router and improve existing code to use router

--- section9/public/index.php
+++ section9/public/index.php
@@ -6,4 +6,18 @@ require '../helpers.php';
 // code from that file then becomes available for use - as shown with basePath function
 // require basePath('views/home.view.php');
 
-loadView('home');
+$routes = [
+  '/' => 'controllers/home.php',
+  '/listings' => 'controllers/listings/index.php',
+  '/listings/create' => 'controllers/listings/create.php',
+  '404' => 'controllers/error/404.php'
+];
+
+// grabs the request URL from PHP $_SERVER superglobal
+$uri = $_SERVER['REQUEST_URI'];
+
+if (array_key_exists($uri, $routes)) {
+  require(basePath($routes[$uri]));
+} else {
+  require basePath($routes['404']);
+}
