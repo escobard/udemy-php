b727781cc48ceb8d95da148db4e0ee6763204107
escobard <escobard90@gmail.com>
2025-07-26 09:47:04 -0600

    feat: section 10 starting files

---
+++ section10/views/home.view.php
@@ -0,0 +1,10 @@
+<?php
+
+// functions that were required/imported in a parent file are available in children files
+loadPartial('head');
+loadPartial('navbar');
+loadPartial('showcase-search');
+loadPartial('top-banner');
+loadPartial('listings');
+loadPartial('top-banner');
+loadPartial('footer');
