533928a64a38428df6cd4a53cb8a11adcabffd9e
escobard <escobard90@gmail.com>
2025-08-26 08:38:51 -0600

    feat: login and register routes & views

--- section13/routes.php
+++ section13/routes.php
@@ -12,8 +12,5 @@ $router->put('/listings/{id}', 'ListingController@update');
 
 $router->delete('/listings/{id}', 'ListingController@destroy');
 
-// $router->get('/', 'controllers/home.php');
-// $router->get('/listings', 'controllers/listings/index.php');
-// $router->get('/listings/create', 'controllers/listings/create.php');
-
-// $router->get('/listing', 'controllers/listings/show.php');
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
