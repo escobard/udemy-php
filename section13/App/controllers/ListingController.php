0f8cc35ba91cfe03ffa84717a7b883d7935968b0
escobard <escobard90@gmail.com>
2025-08-27 08:35:56 -0600

    feat: prevent update unless user is owner

--- section13/App/controllers/ListingController.php
+++ section13/App/controllers/ListingController.php
@@ -183,6 +183,11 @@ class ListingController
 
     $listing = $this->db->query('SELECT * FROM workopia.listings WHERE id = :id', $params)->fetch();
 
+    if (!Authorization::isOwner($listing['user_id'])) {
+      Session::setFlashMessage('error_message', 'You are not authorized to delete this listing');
+      return redirect('/listings/' . $listing['id']);
+    }
+
     loadView('listings/edit', ['listing' => $listing]);
   }
 
@@ -208,6 +213,11 @@ class ListingController
     if (!$listing) {
       ErrorController::notFound('Listing not found');
       return;
+    }
+
+    if (!Authorization::isOwner($listing['user_id'])) {
+      Session::setFlashMessage('error_message', 'You are not authorized to update this listing');
+      return redirect('/listings/' . $listing['id']);
     }
 
     $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
